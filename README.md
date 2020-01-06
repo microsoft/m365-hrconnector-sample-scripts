07e0157dd0f75a880f9ac7813c9fe2120d8f2d71
nirav pranami <nirav.pranami@microsoft.com>
2020-01-06 12:31:20 +0530

    add the powershell script to upload termination records

--- README.md
+++ README.md
@@ -1,14 +1,54 @@
+The script would read data from a csv file and upload it to the configured **hr connector** job.
 
-# Contributing
+## Creating AAD APP
 
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
+Create you AAD APP https://docs.microsoft.com/en-us/azure/kusto/management/access-control/how-to-provision-aad-app
 
-When you submit a pull request, a CLA bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
+ Note the following
+ - APP_ID (aka Application ID or Client)
+ - APP_SECRET (aka client secret)
+ - TENANT_ID (aka directory ID)
 
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+## Create your job on SCC
+
+Provide required Details at HR Connector and create a job and note the **JOB_ID**
+
+## Run the powershell script
+```powershell
+.\upload_csv_for_hr_termination.ps1 -tenantId <TENANT_ID> -appId <APP_ID>  -appSecret <APP_SECRET>  -jobId <JOB_ID>  -csvFilePath <CSV_FILE_PATH>
+```
+
+CSV_FILE_PATH should be the local file path for the csv data to upload.
+
+ If the last line reads **Upload Successful**, the script execution was successful. 
+
+The script would retry  over a period of about 15 mins if it encounters any transient failures.
+
+## CSV criteria
+
+- the columns names should match the ones setup while creating a job
+
+- the dates should be in `yyyy-mm-ddThh:mm:ss.nnnnnn+|-hh:mm` format.  Sample dates as below
+
+  - 2008-09-15T15:53:00+05:00 
+  - 2008-09-15T15:53:00Z
+
+  #### Sample CSV
+
+  | ResignationDate                   | UserEmailAddress       | LastWorkingDate                   |
+  | --------------------------------- | ---------------------- | --------------------------------- |
+  | 2019-04-29T15:18:02.4675041+05:30 | testing1@microsoft.com | 2019-04-23T15:18:02.4675041+05:30 |
+  | 2019-04-24T09:15:49Z              | testing2@microsoft.com | 2019-04-23T15:18:02.4675041       |
+
+## Common Errors and resolution
+
+1. JOB_ID might be incorrect. Make sure it matches the one configured on SCC
+
+> RetryCommand : Failure with StatusCode [Forbidden] and ReasonPhrase [jobId and corresponding appId do not match.]
+> 
+
+2. APP_ID or TENANT_ID might be incorrect.
+
+> RetryCommand : {"error":"unauthorized_client","error_description":"AADSTS700016: Application with identifier '689412fa-4b24-475a-ab39-32eca848b6f2' was 
+> not found in the directory '85ee0691-54d7-49f1-b879-3ce53c2a8549'. This can happen if the application has not been installed by the administrator of the 
+> tenant or consented to by any user in the tenant. You may have sent your authentication request to the wrong tenant.}
---
+++ upload_termination_records.ps1
@@ -0,0 +1,148 @@
+param
+(   
+    [Parameter(mandatory = $true)]
+    [string] $tenantId,
+    [Parameter(mandatory = $true)]
+    [string] $appId,
+    [Parameter(mandatory = $true)]
+    [string] $appSecret,
+    [Parameter(mandatory = $true)]
+    [string] $jobId,
+    [Parameter(mandatory = $true)]
+    [string] $csvFilePath
+)
+# Access Token Config
+$oAuthTokenEndpoint = "https://login.windows.net/$tenantId/oauth2/token"
+# TODO https://o365exchange.visualstudio.com/IP%20Engineering/_workitems/edit/1280218
+$resource = 'https://microsoft.onmicrosoft.com/4e476d41-2395-42be-89ff-34cb9186a1ac'
+
+# Csv upload config
+$eventApiURl = "https://webhook-df.ingestion.office.com"
+$eventApiEndpoint = "api/signals/hr/termination/csv"
+
+function GetAccessToken () {
+    # Token Authorization URI
+    $uri = "$($oAuthTokenEndpoint)?api-version=1.0"
+
+    # Access Token Body
+    $formData = 
+    @{
+        client_id     = $appId;
+        client_secret = $appSecret;
+        grant_type    = 'client_credentials';
+        resource      = $resource;
+        tenant_id     = $tenantId;
+    }
+
+    # Parameters for Access Token call
+    $params = 
+    @{
+        URI         = $uri
+        Method      = 'Post'
+        ContentType = 'application/x-www-form-urlencoded'
+        Body        = $formData
+    }
+
+    $response = Invoke-RestMethod @params -ErrorAction Stop
+    return $response.access_token
+}
+
+function RetryCommand {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position = 0, Mandatory = $true)]
+        [scriptblock]$ScriptBlock,
+
+        [Parameter(Position = 1, Mandatory = $false)]
+        [int]$Maximum = 15
+    )
+
+    Begin {
+        $cnt = 0
+    }
+
+    Process {
+        do {
+            $cnt++
+            try {
+                $ScriptBlock.Invoke()
+                return
+            }
+            catch {
+                Write-Error $_.Exception.InnerException.Message -ErrorAction Continue
+                Start-Sleep 60
+                if ($cnt -lt $Maximum) {
+                    Write-Output "Retrying"
+                }
+            }
+            
+        } while ($cnt -lt $Maximum)
+
+        throw 'Execution failed.'
+    }
+}
+
+function WriteErrorMessage($errorMessage) {
+    $Exception = [Exception]::new($errorMessage)
+    $ErrorRecord = [System.Management.Automation.ErrorRecord]::new(
+        $Exception,
+        "errorID",
+        [System.Management.Automation.ErrorCategory]::NotSpecified,
+        $TargetObject
+    )
+    $PSCmdlet.WriteError($ErrorRecord)
+}
+
+function UploadCsvData ($access_token) {
+    $nvCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty) 
+    $nvCollection.Add('jobid', $jobId)
+    $uriRequest = [System.UriBuilder]"$eventApiURl/$eventApiEndpoint"
+    $uriRequest.Query = $nvCollection.ToString()
+
+    $fieldName = 'file'
+    $url = $uriRequest.Uri.OriginalString
+
+    Add-Type -AssemblyName 'System.Net.Http'
+
+    $client = New-Object System.Net.Http.HttpClient
+    $content = New-Object System.Net.Http.MultipartFormDataContent
+    try{
+        $fileStream = [System.IO.File]::OpenRead($csvFilePath)
+        $fileName = [System.IO.Path]::GetFileName($csvFilePath)
+        $fileContent = New-Object System.Net.Http.StreamContent($fileStream)
+        $content.Add($fileContent, $fieldName, $fileName)
+    }catch{
+        WriteErrorMessage("Error reading from file. Please check if path is correct")
+        return
+    }
+    $client.DefaultRequestHeaders.Add("Authorization", "Bearer $access_token");
+    $client.Timeout = New-Object System.TimeSpan(0, 0, 150)
+    
+    try {
+        $result = $client.PostAsync($url, $content).Result
+    }
+    catch {
+        WriteErrorMessage("Unknown failure while uploading.")
+        return
+    }
+    $status_code = [int]$result.StatusCode
+    if ($result.IsSuccessStatusCode) {
+        Write-Output "Upload Successful"
+        $responseStr = $result.Content.ReadAsStringAsync().Result
+        if (! [string]::IsNullOrWhiteSpace($responseStr)) {
+            Write-Output("Body : {0}" -f $responseStr)
+        }
+    }
+    elseif ($status_code -eq 0 -or $status_code -eq 501 -or $status_code -eq 503) {
+        throw "Service unavailable."
+    }
+    else {
+        WriteErrorMessage("Failure with StatusCode [{0}] and ReasonPhrase [{1}]" -f $result.StatusCode, $result.ReasonPhrase)
+        WriteErrorMessage("Error body : {0}" -f $result.Content.ReadAsStringAsync().Result)
+    }
+}
+
+RetryCommand -ScriptBlock {
+    $access_token = GetAccessToken
+    UploadCsvData($access_token)
+}
